98773f8047d61f6a20663d7be2bdc0eda9ce5334
Julien Kauffmann <julien.kauffmann@freelan.org>
2016-05-29 19:26:38 -0400

    Added a basic implementation of an inproc connection

--- azmq/connection.py
+++ azmq/connection.py
@@ -53,6 +53,23 @@ class BaseConnection(ClosableAsyncObject):
         self.on_lost = on_lost
         self.subscriptions = []
         self._discard_incoming_messages = False
+        self._run_task = asyncio.ensure_future(self.run(), loop=self.loop)
+
+    async def on_close(self, result):
+        # When the closing state is set, all tasks in the run task are
+        # guaranteed to stop, so we can just wait gracefully for it to happen.
+        await self._run_task
+        await self.unsubscribe_all()
+
+        if self.outbox:
+            self.outbox.close()
+            await self.outbox.wait_closed()
+
+        if self.inbox:
+            self.inbox.close()
+            await self.inbox.wait_closed()
+
+        return result
 
     def set_queues(self, inbox, outbox):
         self.inbox = inbox
@@ -120,6 +137,140 @@ class BaseConnection(ClosableAsyncObject):
         )
 
 
+class InprocConnection(BaseConnection):
+    """
+    Implements a ZMTP connection that works in-process.
+    """
+    def __init__(
+        self,
+        in_channel,
+        out_channel,
+        attributes,
+        on_ready,
+        on_lost,
+        **kwargs
+    ):
+        super().__init__(
+            attributes=attributes,
+            on_ready=on_ready,
+            on_lost=on_lost,
+            **kwargs,
+        )
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+
+    async def run(self):
+        try:
+            await self.on_run()
+        except Exception:
+            logger.exception("Unexpected error. Terminating connection.")
+        finally:
+            self.close()
+
+    async def on_run(self):
+        await self.out_channel.write(b'NULL')
+        mechanism = await self.in_channel.read()
+
+        if mechanism == b'NULL':
+            await self.out_channel.write(
+                {
+                    b'socket-type': self.local_socket_type,
+                    b'identity': self.local_identity,
+                },
+            )
+            peer_attributes = await self.in_channel.read()
+            logger.debug("Peer attributes: %s", peer_attributes)
+
+            if (
+                self.local_socket_type,
+                peer_attributes[b'socket-type'],
+            ) not in LEGAL_COMBINATIONS:
+                logger.warning(
+                    "Incompatible socket types (%s <-> %s). Killing the "
+                    "connection.",
+                    self.local_socket_type.decode(),
+                    peer_attributes[b'socket-type'].decode(),
+                )
+                return
+
+            self.identity = peer_attributes.get(b'identity', None)
+
+            if self.identity:
+                # Peer-specified identities can't start with b'\x00'.
+                if not self.identity[0]:
+                    logger.warning(
+                        "Peer specified an invalid identity (%r). Killing the "
+                        "connection.",
+                        self.identity,
+                    )
+                    return
+        else:
+            logger.warning("Unsupported mechanism: %s.", mechanism)
+            return
+
+        logger.debug("Connection is now ready to read and write.")
+        self.on_ready(self)
+        assert self.inbox or self.outbox, (
+            "on_ready callback must either set an inbox or an outbox."
+        )
+
+        try:
+            tasks = []
+
+            if self.inbox:
+                read_task = asyncio.ensure_future(self.read(), loop=self.loop)
+                tasks.append(read_task)
+
+            if self.outbox:
+                write_task = asyncio.ensure_future(
+                    self.write(),
+                    loop=self.loop,
+                )
+                tasks.append(write_task)
+
+            try:
+                await self.await_until_closing(
+                    asyncio.gather(*tasks, loop=self.loop),
+                )
+            except asyncio.CancelledError:
+                logger.debug(
+                    "Read/write cycle interrupted. Connection will die soon.",
+                )
+            finally:
+                for task in tasks:
+                    task.cancel()
+
+                await asyncio.wait(tasks, loop=self.loop)
+        finally:
+            self.on_lost(self)
+
+        # Flush out the unset outgoing messages before we exit.
+        while not self.outbox.empty():
+            write_frames(self.writer, self.outbox.read_nowait())
+
+    async def read(self):
+        while not self.closing:
+            frames = await self.in_channel.read()
+
+            if self.local_socket_type in {PUB, XPUB}:
+                type_ = frames[0][0]
+
+                if type_ == 1:
+                    await self.subscribe(frames[0][1:])
+                elif type_ == 0:
+                    await self.unsubscribe(frames[0][1:])
+            else:
+                if not self._discard_incoming_messages:
+                    await self.inbox.write(frames)
+
+    async def write(self):
+        outbox = self.outbox
+
+        while not self.closing:
+            frames = await outbox.read()
+            await self.out_channel.write(frames)
+
+
 class StreamConnection(BaseConnection):
     """
     Implements a ZMTP connection that works on a pair of streams.
@@ -141,23 +292,6 @@ class StreamConnection(BaseConnection):
         )
         self.reader = reader
         self.writer = writer
-        self.run_task = asyncio.ensure_future(self.run(), loop=self.loop)
-
-    async def on_close(self, result):
-        # When the closing state is set, all tasks in the run task are
-        # guaranteed to stop, so we can just wait gracefully for it to happen.
-        await self.run_task
-        await self.unsubscribe_all()
-
-        if self.outbox:
-            self.outbox.close()
-            await self.outbox.wait_closed()
-
-        if self.inbox:
-            self.inbox.close()
-            await self.inbox.wait_closed()
-
-        return result
 
     async def run(self):
         try:
