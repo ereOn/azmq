52560f1f9f29b450fe7dc216658e21ff667d0b1c
Julien Kauffmann <julien.kauffmann@freelan.org>
2016-05-29 18:34:42 -0400

    Factorized common connection code

--- azmq/connection.py
+++ azmq/connection.py
@@ -32,52 +32,31 @@ from .messaging import (
 )
 
 
-class StreamConnection(ClosableAsyncObject):
+class BaseConnection(ClosableAsyncObject):
     """
-    Implements a ZMTP connection that works on a pair of streams.
+    The base class for ZMTP connections.
     """
     def __init__(
         self,
-        reader,
-        writer,
         attributes,
         on_ready,
         on_lost,
         **kwargs
     ):
         super().__init__(**kwargs)
-        self.reader = reader
-        self.writer = writer
+        self.inbox = None
+        self.outbox = None
+        self.identity = None
         self.local_socket_type = attributes['socket_type']
         self.local_identity = attributes.get('identity', b'')
         self.on_ready = on_ready
         self.on_lost = on_lost
-        self.identity = None
-        self.run_task = asyncio.ensure_future(self.run(), loop=self.loop)
-        self.inbox = None
-        self.outbox = None
-        self._discard_incoming_messages = False
         self.subscriptions = []
+        self._discard_incoming_messages = False
 
     def set_queues(self, inbox, outbox):
         self.inbox = inbox
         self.outbox = outbox
-
-    async def on_close(self, result):
-        # When the closing state is set, all tasks in the run task are
-        # guaranteed to stop, so we can just wait gracefully for it to happen.
-        await self.run_task
-        await self.unsubscribe_all()
-
-        if self.outbox:
-            self.outbox.close()
-            await self.outbox.wait_closed()
-
-        if self.inbox:
-            self.inbox.close()
-            await self.inbox.wait_closed()
-
-        return result
 
     @contextmanager
     def discard_incoming_messages(self, clear=True):
@@ -99,6 +78,86 @@ class StreamConnection(ClosableAsyncObject):
             yield
         finally:
             self._discard_incoming_messages = previous
+
+    @cancel_on_closing
+    async def local_subscribe(self, topic):
+        logger.debug("Subscribed to topic %r.", topic)
+
+        if self.outbox:
+            await self.outbox.write([b'\x01' + topic])
+
+    @cancel_on_closing
+    async def local_unsubscribe(self, topic):
+        logger.debug("Unsubscribed from topic %r.", topic)
+
+        if self.outbox:
+            await self.outbox.write([b'\x00' + topic])
+
+    async def subscribe(self, topic):
+        self.subscriptions.append(topic)
+        logger.debug("Peer subscribed to topic %r.", topic)
+
+        # XPUB sockets must inform the application of the subscription.
+        if self.local_socket_type == XPUB:
+            if self.inbox:
+                await self.inbox.write([b'\x01' + topic])
+
+    async def unsubscribe(self, topic):
+        try:
+            self.subscriptions.remove(topic)
+            logger.debug("Peer unsubscribed from topic %r.", topic)
+        except ValueError:
+            pass
+        else:
+            # XPUB sockets must inform the application of the unsubscription.
+            if self.local_socket_type == XPUB:
+                if self.inbox:
+                    await self.inbox.write([b'\x00' + topic])
+
+    async def unsubscribe_all(self):
+        await asyncio.gather(
+            *[self.unsubscribe(topic) for topic in self.subscriptions[:]]
+        )
+
+
+class StreamConnection(BaseConnection):
+    """
+    Implements a ZMTP connection that works on a pair of streams.
+    """
+    def __init__(
+        self,
+        reader,
+        writer,
+        attributes,
+        on_ready,
+        on_lost,
+        **kwargs
+    ):
+        super().__init__(
+            attributes=attributes,
+            on_ready=on_ready,
+            on_lost=on_lost,
+            **kwargs,
+        )
+        self.reader = reader
+        self.writer = writer
+        self.run_task = asyncio.ensure_future(self.run(), loop=self.loop)
+
+    async def on_close(self, result):
+        # When the closing state is set, all tasks in the run task are
+        # guaranteed to stop, so we can just wait gracefully for it to happen.
+        await self.run_task
+        await self.unsubscribe_all()
+
+        if self.outbox:
+            self.outbox.close()
+            await self.outbox.wait_closed()
+
+        if self.inbox:
+            self.inbox.close()
+            await self.inbox.wait_closed()
+
+        return result
 
     async def run(self):
         try:
@@ -224,46 +283,6 @@ class StreamConnection(ClosableAsyncObject):
         # Flush out the unset outgoing messages before we exit.
         while not self.outbox.empty():
             write_frames(self.writer, self.outbox.read_nowait())
-
-    @cancel_on_closing
-    async def local_subscribe(self, topic):
-        logger.debug("Subscribed to topic %r.", topic)
-
-        if self.outbox:
-            await self.outbox.write([b'\x01' + topic])
-
-    @cancel_on_closing
-    async def local_unsubscribe(self, topic):
-        logger.debug("Unsubscribed from topic %r.", topic)
-
-        if self.outbox:
-            await self.outbox.write([b'\x00' + topic])
-
-    async def subscribe(self, topic):
-        self.subscriptions.append(topic)
-        logger.debug("Peer subscribed to topic %r.", topic)
-
-        # XPUB sockets must inform the application of the subscription.
-        if self.local_socket_type == XPUB:
-            if self.inbox:
-                await self.inbox.write([b'\x01' + topic])
-
-    async def unsubscribe(self, topic):
-        try:
-            self.subscriptions.remove(topic)
-            logger.debug("Peer unsubscribed from topic %r.", topic)
-        except ValueError:
-            pass
-        else:
-            # XPUB sockets must inform the application of the unsubscription.
-            if self.local_socket_type == XPUB:
-                if self.inbox:
-                    await self.inbox.write([b'\x00' + topic])
-
-    async def unsubscribe_all(self):
-        await asyncio.gather(
-            *[self.unsubscribe(topic) for topic in self.subscriptions[:]]
-        )
 
     async def read(self):
         frames = []
